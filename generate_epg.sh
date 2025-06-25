dcf28f2d040780f2d6e8d2e52e9d63ade6af1a28
mikeearle <mearle@aio-tv.com>
2025-06-25 17:17:38 -0600

    Update generate_epg.sh

--- generate_epg.sh
+++ generate_epg.sh
@@ -9,4 +9,5 @@ cd epg
 npm install
 
 # Run grabber (pulls all site configs and merges into one guide.xml)
-npm run grab -- --channels=../verified_usa_can_mex.m3u --output=../guide.xml
+npm run grab -- --site=us --output=../guide.xml
+
